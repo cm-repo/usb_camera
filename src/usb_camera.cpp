6d209cc94267ae8b0e3b3f9d9eebf1d96b42a75f
Chao Qu <quchao@seas.upenn.edu>
2014-07-21 18:40:14 -0400

    add fps control

--- src/usb_camera.cpp
+++ src/usb_camera.cpp
@@ -77,6 +77,7 @@ void UsbCamera::ReconfigureCallback(usb_camera::UsbCameraDynConfig &config,
     return;
   }
   // Get config
+  SetRate(config.fps);
   UsbCameraConfig new_config;
   new_config.color = config.color;
   Configure(new_config);
