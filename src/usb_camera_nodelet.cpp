02a33165f3be271923003b71d751ac592f9bb73d
Chao Qu <quchao@seas.upenn.edu>
2014-07-22 13:44:58 -0400

    merge nodelet interface and implementation

--- src/usb_camera_nodelet.cpp
+++ src/usb_camera_nodelet.cpp
@@ -1,22 +1,26 @@
-#include "usb_camera/usb_camera_nodelet.h"
+#include "usb_camera/usb_camera.h"
 
+#include <ros/ros.h>
 #include <nodelet/nodelet.h>
 #include <pluginlib/class_list_macros.h>
 
-PLUGINLIB_DECLARE_CLASS(usb_camera, UsbCameraNodelet,
-                        usb_camera::UsbCameraNodelet, nodelet::Nodelet)
-
 namespace usb_camera {
 
-UsbCameraNodelet::UsbCameraNodelet() : nodelet::Nodelet() {}
+class UsbCameraNodelet : public nodelet::Nodelet {
+ public:
+  UsbCameraNodelet() : nodelet::Nodelet() {};
+  ~UsbCameraNodelet() { usb_camera_->End(); }
 
-UsbCameraNodelet::~UsbCameraNodelet() {
-  usb_camera_->End();
-}
+  virtual void onInit() {
+    usb_camera_.reset(new UsbCamera(getPrivateNodeHandle()));
+    usb_camera_->Run();
+  }
 
-void UsbCameraNodelet::onInit() {
-  usb_camera_.reset(new UsbCamera(getPrivateNodeHandle()));
-  usb_camera_->Run();
-}
+ private:
+  std::unique_ptr<usb_camera::UsbCamera> usb_camera_;
+};
+
+PLUGINLIB_DECLARE_CLASS(usb_camera, UsbCameraNodelet,
+                        usb_camera::UsbCameraNodelet, nodelet::Nodelet)
 
 }  // namespace usb_camera
